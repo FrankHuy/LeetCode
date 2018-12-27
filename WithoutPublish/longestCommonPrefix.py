6d9c96d585b01d550c0fee5605bf3e35eca02300
FrankHuy <879647324@qq.com>
2018-12-27 18:36:49 +0800

    Create longestCommonPrefix.py

---
+++ WithoutPublish/longestCommonPrefix.py
@@ -0,0 +1,33 @@
+class Solution(object):
+
+    def __init__(self):
+        self.rstr_ = ''
+
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if(strs.__len__() == 0):
+            return self.rstr_
+        elif(strs.__len__() == 1):
+            return strs[0]
+        minitem = min(strs,key = lambda x:len(x))
+        minlen = len(minitem)
+        for index in range(minlen):
+            for i in range(len(strs)-1):
+                if(strs[i][index] != strs[i+1][index]):
+                    return self.rstr_
+            self.rstr_ += strs[0][index]
+        return self.rstr_
+
+
+def main():
+    a = Solution()
+    strs = ['flow' , 'fly' , 'flight']
+    st = a.longestCommonPrefix(strs)
+    print(st)
+
+
+if __name__ == "__main__":
+    main()
